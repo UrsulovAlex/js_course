c9693471f41c423eaeb71c4e0163fe26052a753b
ursulov <ursulov@rozetka.com.ua>
2020-12-24 17:08:55 +0200

    scripts

---
+++ script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if(newFilm) {
+
+            if(newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+                
+            }
+
+            if(favorite) {
+                console.log("добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArray(movieDB.movies);
+    
+            creatMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+    });
+    
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove()
+        })
+    }
+
+    
+
+    const makeCanges = () => {
+        genre.textContent = 'драма';
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    }
+
+    
+    const sortArray = (arr) => {
+        movieDB.movies.sort();
+    }
+    
+
+    function creatMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArray(films);
+
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i+1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        })
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                creatMovieList(films, parent);
+            })
+        })
+    }
+
+    deleteAdv(adv);
+    makeCanges();
+
+    creatMovieList(movieDB.movies, movieList);
+})
